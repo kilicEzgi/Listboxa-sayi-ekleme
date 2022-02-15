e03db8790ddef3e9c10c828b851df1b16140ba7d
kilicEzgi <99246213+kilicEzgi@users.noreply.github.com>
2022-02-15 11:55:29 +0300

    Add files via upload

---
+++ WindowsFormsApplication31/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int sayac = 1;
+            int sayi = Convert.ToInt32(textBox1.Text);
+            int sayi2=Convert.ToInt32(textBox2.Text);
+            while (sayac <= sayi)
+            {
+                listBox1.Items.Add(sayac+sayi2);
+                sayac++;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
